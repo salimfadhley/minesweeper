637faaf136548dc104be9de2e06016faa84b03a4
Abigail Brady <abigail.brady@filigree.tech>
2018-09-06 20:27:37 +0100

    MOONSWEEPER

---
+++ src/minesweeper/minesweeper.py
@@ -0,0 +1,91 @@
+import sys
+
+import gamestate
+
+BOMB_GRAPHIC = "*"
+SPACE_GRAPHIC = "."
+UNREVEALED_GRAPHIC = "?"
+
+def drawBoard(board: gamestate.GameState, fog = True):
+    print("\033c MOONSWEEPER")
+
+    headerText = "  "
+    for x in range(board.board_size):
+        headerText += str(x)
+
+    print(headerText)
+
+    for y in range(board.board_size):
+        
+        rowText = str(y) + " "
+
+        for x in range(board.board_size):
+
+            cell = board.get_state_of_cell(gamestate.Coordinate(x, y))
+
+            if fog and not cell.revealed:
+                rowText += UNREVEALED_GRAPHIC
+
+            elif cell.mine:
+                rowText += BOMB_GRAPHIC
+
+            elif cell.adjacent == 0:
+                rowText += SPACE_GRAPHIC
+            else:
+                rowText += str(cell.adjacent)
+
+        print(rowText)
+
+    sys.stdout.flush()
+
+    pass
+
+def makeMove(board, x, y):
+    return board.doMove(gamestate.Coordinate(x, y))
+
+def isWon(board):
+    return False
+
+
+# board = engine.makeBoard()
+
+board = gamestate.GameState(5, 0.2)
+
+while not isWon(board):
+
+    drawBoard(board)
+
+    command = sys.stdin.readline()
+
+    command = command.rstrip()
+
+    if command == "quit":
+        sys.exit()
+
+    if len(command) > 2:
+        print("coordinate too long")
+        continue
+
+    if len(command) < 2:
+        print("coordinate too short")
+        continue
+
+    try:
+        x = int(command[0])
+        y = int(command[1])
+
+    except:
+        print("invalid input")
+    
+    try:
+        board = makeMove(board, x, y)
+    except:
+
+        drawBoard(board, fog = False)
+
+        print("BANG YOU DIED.")
+
+drawBoard(board)
+
+if isWon(board):
+    print("YAY")
